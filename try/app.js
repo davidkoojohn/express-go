c7e4a59d43e33b0fddf7630181396ab88b8ff202
davidkoojohn <davidkoojohn@gmail.com>
2017-11-28 17:36:52 +0800

    try 使用回调函数数组处理路由：

--- try/app.js
+++ try/app.js
@@ -15,6 +15,23 @@ app.get('/example/b', function (req, res, next) {
 });
 
 
+var cb0 = function (req, res, next) {
+  console.log('CB0');
+  next();
+}
+
+var cb1 = function (req, res, next) {
+  console.log('CB1');
+  next();
+}
+
+var cb2 = function (req, res) {
+  res.send('Hello from C!');
+}
+
+app.get('/example/c', [cb0, cb1, cb2]);
+
+
 var server = app.listen(7777, function () {
 
   var host = server.address().address
