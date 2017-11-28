e0d0f3f1fdad5917021b5020db4fe08b13371723
davidkoojohn <davidkoojohn@gmail.com>
2017-11-28 17:07:36 +0800

    try express.router

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+// 网站首页接受 POST 请求
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render(
+
+    'index',
+    {
+      title: 'Express'
+    }
+  );
 });
+
 
 module.exports = router;
