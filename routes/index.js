1c489062e23166420c000c3a84a57dc6e8cefc4b
davidkoojohn <davidkoojohn@gmail.com>
2017-11-28 17:28:31 +0800

    try / express.router

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
-var router = express.Router();
+
+var app = express()
 
 /* GET home page. */
-// 网站首页接受 POST 请求
-router.get('/', function(req, res, next) {
+// 网站首页接受 GET 请求
+app.get('/', function(req, res, next) {
   res.render(
 
     'index',
@@ -13,5 +14,9 @@ router.get('/', function(req, res, next) {
   );
 });
 
+// 网站首页接受 POST 请求
+app.post('/', function(req, res) {
+  res.send('POST request to the homepage');
+});
 
-module.exports = router;
+module.exports = app;
