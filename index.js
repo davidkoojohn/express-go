077e80449365f22f5bdcf9a0d9c8078860f28f2d
davidkoojohn <davidkoojohn@gmail.com>
2018-01-06 10:47:01 +0800

    used Router

--- index.js
+++ index.js
@@ -2,20 +2,11 @@
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('hello, express')
-})
+const IndexRoute = require('./routes/index')
+const UsersRoute = require('./routes/users')
 
-app.get('/users', function (req, res) {
-  // req.params
-  // console.log(req.params)
-  // res.send('hello, ' + req.params.name)
-
-  // req.query
-  console.log(req.query)
-  res.send('hello, ' + req.query.name)
-
-})
+app.use('/', IndexRoute)
+app.use('/users', UsersRoute)
 
 
 app.listen(7777)
