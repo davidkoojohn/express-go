5c50fddf575d873ae78afb0d25f8fe6c0aef0eae
davidkoojohn <davidkoojohn@gmail.com>
2018-01-06 11:21:56 +0800

    used view Ejs

--- index.js
+++ index.js
@@ -2,6 +2,12 @@
 const express = require('express')
 const app = express()
 
+const path = require('path')
+
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+
+
 const IndexRoute = require('./routes/index')
 const UsersRoute = require('./routes/users')
 
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:name', function (req, res) {
-  res.send('hello, ' + req.params.name)
+  res.render('users', {
+    name: req.params.name,
+    html: '<h1>kooo</h1>',
+    supplies: ['mop', 'broom', 'duster']
+  })
 })
 
 module.exports = router
